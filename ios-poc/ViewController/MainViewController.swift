5216c77d7b1e2f9eda22ae4c11440c82f9f934bb
Syed Khalil Ullah <khalil.ullah@automicgroup.com.au>
2020-04-06 23:37:00 +1000

    Removed storyboards to create UI from code

---
+++ ios-poc/ViewController/MainViewController.swift
@@ -0,0 +1,12 @@
+//Created on 6/4/20
+
+import Foundation
+import UIKit
+
+class MainViewController: UINavigationController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
